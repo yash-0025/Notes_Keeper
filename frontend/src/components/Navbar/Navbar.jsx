cbf449aaa55dc236a3d9becab2d21fd4b2d9abad
yash-0025 <yashpatel200403@gmail.com>
2025-05-07 13:26:08 +0530

    #FRONTEND :: SEARCH-BAR  CREATED✅

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProfileCard from '../Cards/ProfileCard'
+import SearchBar
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
